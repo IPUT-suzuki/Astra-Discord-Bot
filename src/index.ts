63ca6e9c8a16bfb52a0ac63655ca6b4747e0e792
Ibuki Suzuki <belle.tree.japan@gmail.com>
2025-12-09 07:33:20 +0000

    コード書き換え

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import 'dotenv/config';
+import { Client, IntentsBitField } from 'discord.js';
+import type { Interaction } from 'discord.js';
+import { Log } from './utils/logger.js';
+
+const args = process.argv.slice(2);
+export const isDebug = args.includes('debug');
+if (isDebug) {
+    Log.main('Boot is debug mode');
+}
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+        IntentsBitField.Flags.GuildVoiceStates,
+    ],
+});
+
+client.login(process.env.TOKEN);
+
+client.on('clientReady', async (c) => {
+    Log.main(c.user.tag + 'is Online');
+});
+
+client.on('interactionCreate', async (interaction: Interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+    // ここから下はinteraction: ChatInputCommandInteractionとして扱える
+    const command = interaction.commandName;
+    const subCommand = interaction.options.getSubcommand(false);
+    const fullcommand = subCommand ? command + ' ' + subCommand : command;
+    Log.useCommand(interaction);
+    if (fullcommand == 'valo rank') {
+    } else if (fullcommand == 'valo team') {
+    } else if (fullcommand == 'valo map') {
+    } else if (fullcommand == 'valo list') {
+    } else if (fullcommand == 'dice') {
+    }
+});
