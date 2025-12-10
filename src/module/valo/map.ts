8886192f30ffd7820f4bad63a66e689abb593418
Ibuki Suzuki <belle.tree.japan@gmail.com>
2025-12-10 14:34:54 +0000

    exclude オプション用の処理を追加

--- src/module/valo/map.ts
+++ src/module/valo/map.ts
@@ -1,39 +1,83 @@
-import { Colors, EmbedBuilder, type ChatInputCommandInteraction } from 'discord.js';
+import {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    Colors,
+    EmbedBuilder,
+    type ChatInputCommandInteraction,
+} from 'discord.js';
 import { mapInfo } from '../../utils/valoconfig.js';
 import type { slashValoMapData, MapInfo } from '../../utils/interface.js';
 import { Log } from '../../utils/logger.js';
+import { listener } from './common/listener.js';
 
 export class ValoMap {
     i: ChatInputCommandInteraction;
     data: slashValoMapData;
     mapinfo: MapInfo[];
+    excludeConfirm: boolean;
     constructor(interaction: ChatInputCommandInteraction) {
         this.i = interaction;
-        this.data = {
-            option: interaction.options.getString('option', true),
-            map: '',
-            mapImage: '',
-            miniMapImage: '',
-        };
-        this.mapinfo = mapInfo;
+        this.data = { option: interaction.options.getString('option', true) };
+        this.mapinfo = mapInfo.map((map) => ({
+            ...map,
+            selected: false,
+        }));
+        this.excludeConfirm = false;
     }
 
     async start() {
         if (this.data.option === 'all') {
-            //何もしない
+            await this.sendResult(true);
         } else if (this.data.option === 'competitive') {
             this.setCompetitiveMap();
+            await this.sendResult(true);
+        } else if (this.data.option === 'exclude') {
+            let isFirst = true;
+            while (!this.excludeConfirm) {
+                await this.sendExcludeOption(isFirst);
+                isFirst = false;
+                const newInteraction = await listener(await this.i.fetchReply(), 'exclude_', null);
+                if (newInteraction?.customId === 'exclude_confirm') {
+                    this.excludeConfirm = true;
+                } else if (newInteraction?.customId?.startsWith('exclude_')) {
+                    const mapName = newInteraction.customId.replace('exclude_', '');
+                    const target = this.mapinfo.find((map) => map.name === mapName);
+                    if (target) {
+                        target.selected = !target.selected;
+                    }
+                }
+            }
+            this.setExcludeMap();
+            await this.sendResult(false);
         }
+    }
+
+    async sendExcludeOption(isFirst: boolean) {
+        const method = isFirst ? 'reply' : 'editReply';
+        await this.i[method]({
+            embeds: [Embed.excludeInfo(this.mapinfo)],
+            components: [
+                ...Button.excluedeButton(this.mapinfo),
+                new ActionRowBuilder<ButtonBuilder>().addComponents(Button.confirmButton()),
+            ],
+        });
+    }
+    async sendResult(isFirst: boolean) {
         this.randomMapSelector();
-        const embed = Embed.result(this.data);
         Log.debug(JSON.stringify(this.data, null, 2));
-        await this.i.reply({
-            embeds: [embed],
+        const method = isFirst ? 'reply' : 'editReply';
+        await this.i[method]({
+            embeds: [Embed.result(this.data)],
+            components: [],
         });
     }
 
     setCompetitiveMap() {
         this.mapinfo = this.mapinfo.filter((map) => map.competitive === true);
+    }
+    setExcludeMap() {
+        this.mapinfo = this.mapinfo.filter((map) => map.selected === false);
     }
 
     randomMapSelector() {
@@ -53,10 +97,54 @@ class Embed {
             .setColor(Colors.Green)
             .setTitle(`マップ抽選結果`)
             .setFields([{ name: `マップ名 : ${data.map}`, value: '', inline: true }])
-            .setImage(data.mapImage)
-            .setThumbnail(data.miniMapImage)
+            .setImage(data.mapImage ?? '')
+            .setThumbnail(data.miniMapImage ?? '')
             .setFooter({ text: `Select command option : ${data.option}` });
+    }
+    static excludeInfo(data: MapInfo[]) {
+        const field = data.map((map) => ({
+            name: (map.selected ? ':red_circle:' : ':blue_circle:') + ' ' + map.name,
+            value: '',
+            inline: true,
+        }));
+        return new EmbedBuilder()
+            .setColor(Colors.Yellow)
+            .setTitle('除外するマップを選択してください')
+            .setFields(field)
+            .setFooter({ text: '🔵 -- 未除外\n🔴 -- 除外済み' });
     }
 }
 
-class Button {}
+class Button {
+    static excluedeButton(data: MapInfo[]) {
+        const unselectedCount = data.filter((map) => !map.selected).length;
+        const buttons: ButtonBuilder[] = data.map((map) => {
+            if (!map.selected && unselectedCount === 1) {
+                return new ButtonBuilder()
+                    .setCustomId(`exclude_${map.name}`)
+                    .setLabel(map.name ?? '')
+                    .setStyle(ButtonStyle.Secondary)
+                    .setDisabled(true);
+            }
+            // 通常のボタン
+            return new ButtonBuilder()
+                .setCustomId(`exclude_${map.name}`)
+                .setLabel(map.name ?? '')
+                .setStyle(map.selected ? ButtonStyle.Danger : ButtonStyle.Primary);
+        });
+
+        const rows: ActionRowBuilder<ButtonBuilder>[] = [];
+        for (let i = 0; i < buttons.length; i += 4) {
+            rows.push(
+                new ActionRowBuilder<ButtonBuilder>().addComponents(...buttons.slice(i, i + 4))
+            );
+        }
+        return rows;
+    }
+    static confirmButton() {
+        return new ButtonBuilder()
+            .setCustomId('exclude_confirm')
+            .setLabel('確定')
+            .setStyle(ButtonStyle.Success);
+    }
+}
