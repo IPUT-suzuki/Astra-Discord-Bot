5027dbbcb911744f05dd7585ceb00b4c2bb4bc1c
Ibuki Suzuki <belle.tree.japan@gmail.com>
2026-01-11 01:22:12 +0000

    一部修正

--- src/module/valo/common/check.ts
+++ src/module/valo/common/check.ts
@@ -2,25 +2,32 @@ import { ChatInputCommandInteraction, Colors, EmbedBuilder, GuildMember } from '
 
 export class Check {
     private static guildCheck(interaction: ChatInputCommandInteraction) {
+        //サーバー内の場合false,サーバー外の場合true
         return !interaction.inGuild();
     }
     private static vcCheck(interaction: ChatInputCommandInteraction) {
+        //vc接続している場合false,接続していない場合true
         return !(interaction.member as GuildMember).voice.channel;
     }
     private static vcUserCountCheck(interaction: ChatInputCommandInteraction, value: number) {
+        //vcに規定の人数がいるかチェック
         const voiceChannel = (interaction.member as GuildMember).voice.channel;
         return voiceChannel!.members.size < value;
     }
 
     static valoTeamCheck(interaction: ChatInputCommandInteraction) {
+        const isExclude = interaction.options.getBoolean('exclude-option', false) ?? false;
         if (this.guildCheck(interaction)) {
             return Embed.noGuild();
         }
         if (this.vcCheck(interaction)) {
             return Embed.noConnectVc();
         }
-        if (this.vcUserCountCheck(interaction, 2)) {
-            return Embed.notEnoughPeopleInVc(2);
+        if (this.vcUserCountCheck(interaction, isExclude ? 3 : 2)) {
+            return Embed.notEnoughPeopleInVc(isExclude ? 3 : 2);
+        }
+        if (!this.vcUserCountCheck(interaction, 25)) {
+            return Embed.overVcUser();
         }
         return null; // 問題なければnull
     }
@@ -37,6 +44,11 @@ class Embed {
         return this.errorEmbed().setDescription('VCに接続されていません\nVCに接続してから実行してください');
     }
     static notEnoughPeopleInVc(value: number) {
-        return this.errorEmbed().setDescription(`同じVCに${value}名以上のユーザーが必要です\n他のユーザーをVCに招待してください`);
+        return this.errorEmbed().setDescription(
+            `同じVC内に${value}名以上のユーザーが必要です\n他のユーザーをVCに招待してください`
+        );
+    }
+    static overVcUser() {
+        return this.errorEmbed().setDescription('同じVC内に25名を超えるユーザーがいます\nVC内の人数を25名以下にしてください');
     }
 }
